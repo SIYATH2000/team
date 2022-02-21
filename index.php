af5872083cd0200df0656fd7beef319aad9f2a0e
MOHAMED SIYATH <87572745+SIYATH2000@users.noreply.github.com>
2022-02-21 13:24:53 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,154 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Our Team</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+</head>
+<body>
+    <div class="container">
+        <div class="card">
+            <div class="card-inner">
+                <div class="front-face">
+                    <img src="images/Siyath1.jpg">
+                </div>
+                <div class="back-face">
+                    <h2>Siyath MSM</h2>
+                    <p>I'm a Web Developer</p>
+                    <div class="social-media">
+                        <a href="#"><i class="fab fa-facebook"></i></a>
+                        <a href="#"><i class="fab fa-twitter"></i></a>
+                        <a href="#"><i class="fab fa-instagram"></i></a>
+                    </div>
+                    <a href="#" class="btn">Resume</a>
+                </div>
+            </div>
+        </div>
+        <div class="card">
+            <div class="card-inner">
+                <div class="front-face">
+                    <img src="images/Asnaff.jpeg">
+                </div>
+                <div class="back-face">
+                    <h2>D M Asnaff</h2>
+                    <p>lorem ipsum</p>
+                    <div class="social-media">
+                        <a href="#"><i class="fab fa-facebook"></i></a>
+                        <a href="#"><i class="fab fa-twitter"></i></a>
+                        <a href="#"><i class="fab fa-instagram"></i></a>
+                    </div>
+                    <a href="#" class="btn">Resume</a>
+                </div>
+            </div>
+        </div>
+        <div class="card">
+            <div class="card-inner">
+                <div class="front-face">
+                    <img src="images/Nafees.jpeg">
+                </div>
+                <div class="back-face">
+                    <h2>Nafees Nismi</h2>
+                    <p>lorem ipsum</p>
+                    <div class="social-media">
+                        <a href="#"><i class="fab fa-facebook"></i></a>
+                        <a href="#"><i class="fab fa-twitter"></i></a>
+                        <a href="#"><i class="fab fa-instagram"></i></a>
+                    </div>
+                    <a href="#" class="btn">Resume</a>
+                </div>
+            </div>
+        </div>
+        <div class="card">
+            <div class="card-inner">
+                <div class="front-face">
+                    <img src="images/Thanish.jpg">
+                </div>
+                <div class="back-face">
+                    <h2>Thanish</h2>
+                    <p>lorem ipsum</p>
+                    <div class="social-media">
+                        <a href="#"><i class="fab fa-facebook"></i></a>
+                        <a href="#"><i class="fab fa-twitter"></i></a>
+                        <a href="#"><i class="fab fa-instagram"></i></a>
+                    </div>
+                    <a href="#" class="btn">Resume</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container-1">
+        <div class="card">
+            <div class="card-inner">
+                <div class="front-face">
+                    <img src="images/Naveen.jpeg">
+                </div>
+                <div class="back-face">
+                    <h2>Naveen</h2>
+                    <p>lorem ipsum</p>
+                    <div class="social-media">
+                        <a href="#"><i class="fab fa-facebook"></i></a>
+                        <a href="#"><i class="fab fa-twitter"></i></a>
+                        <a href="#"><i class="fab fa-instagram"></i></a>
+                    </div>
+                    <a href="#" class="btn">Resume</a>
+                </div>
+            </div>
+        </div>
+        <div class="card">
+            <div class="card-inner">
+                <div class="front-face">
+                    <img src="images/Sulaim.jpg">
+                </div>
+                <div class="back-face">
+                    <h2>Sulaim</h2>
+                    <p>lorem ipsum</p>
+                    <div class="social-media">
+                        <a href="#"><i class="fab fa-facebook"></i></a>
+                        <a href="#"><i class="fab fa-twitter"></i></a>
+                        <a href="#"><i class="fab fa-instagram"></i></a>
+                    </div>
+                    <a href="#" class="btn">Resume</a>
+                </div>
+            </div>
+        </div>
+        <div class="card">
+            <div class="card-inner">
+                <div class="front-face">
+                    <img src="images/Nashath.jpeg">
+                </div>
+                <div class="back-face">
+                    <h2>Nashath</h2>
+                    <p>lorem ipsum</p>
+                    <div class="social-media">
+                        <a href="#"><i class="fab fa-facebook"></i></a>
+                        <a href="#"><i class="fab fa-twitter"></i></a>
+                        <a href="#"><i class="fab fa-instagram"></i></a>
+                    </div>
+                    <a href="#" class="btn">Resume</a>
+                </div>
+            </div>
+        </div>
+        <div class="card">
+            <div class="card-inner">
+                <div class="front-face">
+                    <img src="images/Suraif.jpeg">
+                </div>
+                <div class="back-face">
+                    <h2>Suraif</h2>
+                    <p>lorem ipsum</p>
+                    <div class="social-media">
+                        <a href="#"><i class="fab fa-facebook"></i></a>
+                        <a href="#"><i class="fab fa-twitter"></i></a>
+                        <a href="#"><i class="fab fa-instagram"></i></a>
+                    </div>
+                    <a href="#" class="btn">Resume</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
